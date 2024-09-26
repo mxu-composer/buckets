4de65d2651686796bed804028f9863874e40a3bf
Michael Xu <michael.xu@composer.trade>
2024-09-26 11:10:23 -0400

    filtered data

--- fix.py
+++ fix.py
@@ -1,5 +1,6 @@
 from google.cloud import storage
 from google.auth import default
+import json
 
 # Initialize the Google Cloud Storage client
 credentials, project = default()
@@ -8,8 +9,9 @@ client = storage.Client(credentials=credentials, project=project)
 # List all buckets
 buckets = list(client.list_buckets())
 
+
 # Convert bucket information to JSON format, including soft delete policy
-bucket_info = [
+buckets_info = [
     {
         "name": bucket.name,
         "location": bucket.location,
@@ -22,5 +24,9 @@ bucket_info = [
     for bucket in buckets
 ]
 
-import json
-print(json.dumps(bucket_info, indent=2))
+for bucket in buckets:
+    for rule in bucket.lifecycle_rules:
+        if (isinstance(rule, storage.bucket.LifecycleRuleDelete)):
+            print(bucket.name)
+            for k, v in rule.items():
+                print('\t', k, v)
